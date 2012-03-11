69a6d7d37defeb0a6b167bc125a9a5dd7d60d3ee
Igor Escobar <blog@igorescobar.com>
2012-03-10 21:41:09 -0300

    Corrigindo bug com mascaras sequencias e aprimorando o preenchimento das mascaras.

--- jquery.mask.js
+++ jquery.mask.js
@@ -10,9 +10,8 @@
 
 /*jslint undef: false, browser: false, es5: true, maxerr: 50, indent: 2 */
 
-$(function () {
+(function ($){
   "use strict";
-
   jQuery.fn.extend({
     specialChars: {':': 191, '-': 189, '.': 190, '(': 57, ')': 48, '/': 191, ',': 188, '_': 189, ' ': 32, '+': 187},
     mask: function (Mascara) {
@@ -23,31 +22,42 @@ $(function () {
       });
     },
     preventSpecialKeys: function (e){
-      e = e || window.event;
-      var SpecialChars = $(this).specialChars,
+
+      var e = e || window.event,
+        keyCode = e.keyCode || e.which,
+        SpecialChars = $(this).specialChars,
         oValue = $(this).val(),
         lastTypedChar = oValue.substring(oValue.length-1);
 
-      if (typeof SpecialChars[lastTypedChar] === "number" && e.keyCode == SpecialChars[lastTypedChar])
+      if (typeof SpecialChars[lastTypedChar] === "number" && keyCode === SpecialChars[lastTypedChar])
         e.preventDefault();
 
     },
     apply_mask: function (e, Mascara) {
-      e = e || window.event;
-      var SpecialChars = $(this).specialChars,
-        byPassKeys = [8],
-        keyCode = e.keyCode? e.keyCode : e.charCode,
-        oValue = $(this).val(),
-        lastTypedChar = oValue.substring(oValue.length-1),
-        pressedKeyChar = String.fromCharCode(lastTypedChar.charCodeAt(0)),
-        nowMask = Mascara.charAt(oValue.length-1),
-        replacePattern = oValue.substring(0, oValue.length-1) + '' + nowMask + pressedKeyChar;
+
+      var e = e || window.event,
+        keyCode = e.keyCode || e.which,
+        SpecialChars = $(this).specialChars,
+        byPassKeys = [8,37,38,39,40],
+        oValue = $(this).val() + '' + String.fromCharCode(keyCode),
+        pressedKeyChar = String.fromCharCode(keyCode),
+        nowMask = Mascara.charAt(oValue.length-1);
 
       if ($.inArray(keyCode, byPassKeys) > -1) return true;
+
+      var bufferedMasks  = '',
+        m = oValue.length-1;
+
+      while (typeof SpecialChars[Mascara.charAt(m)] === "number"){
+        bufferedMasks += Mascara.charAt(m);
+        m++;
+      }
+
+      var replacePattern = oValue.substring(0, oValue.length-1) + bufferedMasks;
 
       if (typeof SpecialChars[nowMask] === "number" && pressedKeyChar !== nowMask) {
         $(this).val(replacePattern);
       }
     }
   });
-});
+})(jQuery);
