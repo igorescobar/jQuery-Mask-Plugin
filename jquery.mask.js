25bda2e16073fcecbf3f27a9f80fbef6117359a2
Igor Escobar <blog@igorescobar.com>
2012-03-13 11:04:04 -0300

    Refatorando o código.

--- jquery.mask.js
+++ jquery.mask.js
@@ -15,73 +15,56 @@
 
   var byPassKeys = [8,9,17,37,38,39,40,91],
     specialChars = {':': 191, '-': 189, '.': 190, '(': 57, ')': 48, '/': 191, ',': 188, '_': 189, ' ': 32, '+': 187},
-    specialCharsValues = [191,189,190,57,48,191,188,189,32,187],
-    e, fieldObject, oNewValue, keyCode, keyPressedString, options, oValue;
+    e, m, fieldObject, oValue, oNewValue, oCleanedValue, keyCode, keyPressedString;
 
   $.fn.mask = function (Mascara) {
     if (!$(this).attr('maxlength'))
       $(this).attr('maxlength', Mascara.length);
 
     $(this).live('keyup', function(e){
-      oValue = $(this).val();
-      var lastDigit = oValue.substring(oValue.length-1).replace(/[:\-.()/,_ +]/g, "");
-      var nowMask = Mascara.charAt(oValue.length-1);
-      var cleanedValue = oValue.substring(0,oValue.length-1);
 
-      if (isNaN(parseInt(nowMask, 10)) === false && /\d/.test(lastDigit) === false) {
-        $(this).val(cleanedValue);
-      } else if(nowMask === 'A' && /[a-zA-Z0-9]/.test(lastDigit) === false) {
-        $(this).val(cleanedValue);
-      } else if(nowMask === 'S' && /[a-zA-Z]/.test(lastDigit) === false) {
-        $(this).val(cleanedValue);
-      }
-    });
+      e = e || window.event;
+      keyCode = e.keyCode || e.which;
 
-    $(this).live('keydown', function (e){
-      if (preventSpecialKeys(e, $(this), Mascara))
+      if ($.inArray(keyCode, byPassKeys) >= 0)
         return true;
-      else
-        applyMask(e, $(this), Mascara);
+
+      applyMask(e, $(this), Mascara);
+
     });
-  };
-
-  var preventSpecialKeys = function (e, fieldObject, Mascara) {
-    e = e || window.event;
-    keyCode = e.keyCode || e.which;
-    oValue = fieldObject.val();
-    keyPressedString = String.fromCharCode(keyCode);
-    oNewValue = oValue + keyPressedString;
-    var nowMask = Mascara.charAt(oNewValue.length-1);
-
-    if ($.inArray(keyCode, byPassKeys) >= 0){
-      return true;
-    }
-
-    if ($.inArray(keyCode, specialCharsValues) >= 0){
-      e.preventDefault();
-    }
-
   };
 
   var applyMask = function (e, fieldObject, Mascara) {
     oValue = fieldObject.val();
-    e = e || window.event;
-    keyCode = e.keyCode || e.which;
-    keyPressedString = String.fromCharCode(keyCode);
 
-    var oNewValue = oValue + keyPressedString,
-      nowMask = Mascara.charAt(oNewValue.length-1),
-      bufferedMasks  = '',
-      m = oNewValue.length-1;
+    oNewValue = '';
+    oCleanedValue = oValue.replace(/\W/g, '');
+    m = 0;
 
-    while (typeof specialChars[Mascara.charAt(m)] === "number"){
-      bufferedMasks += Mascara.charAt(m);
+    for (var i = 0; i < oCleanedValue.length; i++){
+      keyPressedString = oCleanedValue.charAt(i);
+
+      var bufferedMasks  = '';
+      while (typeof specialChars[Mascara.charAt(m)] === "number"){
+        bufferedMasks += Mascara.charAt(m);
+        m++;
+      }
       m++;
+
+      oNewValue += (bufferedMasks !== '') ? (bufferedMasks + keyPressedString) : keyPressedString;
+
+      var nowMask = Mascara.charAt(oNewValue.length-1);
+      keyPressedString = oNewValue.charAt(oNewValue.length-1);
+
+      if (isNaN(parseInt(nowMask, 10)) === false && /\d/.test(keyPressedString) === false) {
+        oNewValue = oNewValue.substring(0, oNewValue.length-1);
+      } else if(nowMask === 'A' && /[a-zA-Z0-9]/.test(keyPressedString) === false) {
+        oNewValue = oNewValue.substring(0, oNewValue.length-1);
+      } else if(nowMask === 'S' && /[a-zA-Z]/.test(keyPressedString) === false) {
+        oNewValue = oNewValue.substring(0, oNewValue.length-1);
+      }
     }
 
-    var replacePattern = oNewValue.substring(0, oNewValue.length-1) + bufferedMasks;
-    if (typeof specialChars[nowMask] === "number") {
-      fieldObject.val(replacePattern);
-    }
+    if(oNewValue !== fieldObject.val()) fieldObject.val(oNewValue);
   };
 })(jQuery);
