c8d055fce26741770ef681927e7fc674499aba75
Igor Escobar <blog@igorescobar.com>
2012-03-14 10:08:40 -0300

    Alterando créditos do jquery mask plugin

--- jquery.mask.js
+++ jquery.mask.js
@@ -1,13 +1,35 @@
-/*!
- * jQuery Mask Plugin
- * https://github.com/igorescobar/jQuery-Mask-Plugin
+ /**
+ * jquery.mask.js
+ * @author: igorescobar
+ * @version: 1.1.3
  *
- * Copyright 2012, Igor Escobar (blog.igorescobar.com)
+ * Created by Igor Escobar on 2012-03-10. Please report any bug at http://blog.igorescobar.com
  *
- * Date: Sat Mar 10 03:50:23 2012
+ * Copyright (c) 2012 Igor Escobar http://blog.igorescobar.com
+ *
+ * The MIT License (http://www.opensource.org/licenses/mit-license.php)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
-
-/*jslint undef: false, browser: false, es5: true, maxerr: 50, indent: 2 */
 
 (function ($){
   "use strict";
