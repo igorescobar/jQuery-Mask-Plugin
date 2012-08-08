165384b850a62b9e6de9a21c7493ccdf259b2097
Marcelo Manzan <manzan@gmail.com>
2012-08-07 23:50:45 -0300

    caching conversion of mask to regex

--- jquery.mask.js
+++ jquery.mask.js
@@ -188,16 +188,23 @@
     return string.replace(/\W/g, '');
   };
 
+  function cachedFunction(func) {
+    var cache = {};
+    return function (arg) {
+      return cache[arg] || (cache[arg] = func(arg));
+    };
+  }
+
   var translation = { 0: '(.)', 1: '(.)', 2: '(.)', 3: '(.)', 4: '(.)', 5: '(.)', 6: '(.)', 7: '(.)',
     8: '(.)', 9: '(.)', 'A': '(.)', 'S': '(.)',':': '(:)?', '-': '(-)?', '.': '(\\\.)?', '(': '(\\()?',
     ')': '(\\))?', '/': '(/)?', ',': '(,)?', '_': '(_)?', ' ': '(\\s)?', '+': '(\\\+)?'};
-  function maskToRegex(mask) {
+  var maskToRegex = cachedFunction(function (mask) {
     var regex = '', len = mask.length, fragment, i = 0;
     while ( i < len ) {
       if ( fragment = translation[mask[i++]] ) regex += fragment;
     }
     return new RegExp( regex );
-  };
+  });
 
   // Public API
 
