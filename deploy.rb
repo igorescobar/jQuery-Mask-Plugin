6667ca252ae5be31881455426054fac5407cd225
Igor Escobar <blog@igorescobar.com>
2013-01-17 22:39:56 -0200

    automatic generation jquery plugins manifest file

--- deploy.rb
+++ deploy.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'jsmin'
 
+JQUERY_MANIFEST_FILE = 'mask.jquery.json'
 JMASK_FILE = 'jquery.mask.js'
 JMASK_MIN_FILE = 'jquery.mask.min.js'
 JMASK_VERSION = `stepup version --next-release`.delete("\n")
@@ -10,7 +11,13 @@ abort("No notes, do deal.") if JMASK_VERSION.empty?
 puts '# PUTTING NEW VERSION INSIDE OF JQUERY MASK FILE'
 unversioned_jmask_file = File.open(JMASK_FILE, 'rb') { |file| file.read }
 File.open(JMASK_FILE, 'w') do |file| 
-  file.write(unversioned_jmask_file.gsub(/\* @version: (v[0-9.+]+)/, "\* @version: #{JMASK_VERSION}"))
+  file.write(unversioned_jmask_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION}\""))
+end
+
+puts '# UPGRADING JQUERY PLUGINS MANIFEST FILE'
+jquery_manifest_file = File.open(JQUERY_MANIFEST_FILE, 'rb') { |file| file.read }
+File.open(JQUERY_MANIFEST_FILE, 'w') do |file| 
+  file.write(jquery_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
 end
 
 puts '# GENERATING MIN FILE'
