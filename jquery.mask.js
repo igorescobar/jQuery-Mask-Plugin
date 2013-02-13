d7f8a179b551d4e1dd1981ec35af8b22d2d031f8
Igor Escobar <blog@igorescobar.com>
2013-02-12 22:04:45 -0200

    a little bit of changes to make the code more testable

--- jquery.mask.js
+++ jquery.mask.js
@@ -55,162 +55,152 @@
         
       
       $el.each(function() {
-        mask = resolveDynamicMask(mask, $(this).val(), e, $(this), options);
+        mask = __p.resolveDynamicMask(mask, $(this).val(), e, $(this), options);
         $el.attr('maxlength', mask.length);
         $el.attr('autocomplete', 'off');
 
-        destroyEvents();
-        setOnKeyUp();
-        setOnPaste();    
+        __p.destroyEvents();
+        __p.setOnKeyUp();
+        __p.setOnPaste();    
       });
     };
+
+    var __p = {
+      resolveDynamicMask: function(mask, oValue, e, currentField, options){
+        return typeof mask == "function" ? mask(oValue, e, currentField, options) : mask;
+      },
+      onlyNonMaskChars: function(string) {
+        $.each(plugin.settings.maskChars, function(k,v){
+          string = string.replace(new RegExp(plugin.settings.maskChars[k], 'g'), '')
+        });
+        return string;
+      },
+      onPasteMethod: function(){
+        setTimeout(function(){
+          $el.trigger('keyup');
+        }, 100);
+      },
+      setOnPaste: function() {
+        (__p.hasOnSupport()) ? $el.on("paste", __p.onPasteMethod) : $el.onpaste = __p.onPasteMethod;
+      },
+      setOnKeyUp: function(){
+        $el.keyup(__p.maskBehaviour).trigger('keyup');
+      },
+      hasOnSupport: function() {
+        return $.isFunction($.on);
+      },
+      destroyEvents: function(){
+        $el.unbind('keyup').unbind('onpaste');
+      },
+      maskBehaviour: function(e){
+        e = e || window.event;
+        keyCode = e.keyCode || e.which;
+
+        if ($.inArray(keyCode, plugin.settings.byPassKeys) > -1)  return true;
+
+        var oCleanedValue = __p.onlyNonMaskChars($el.val()),
+            nowDigitIndex = $el.val().length-1,
+            nowDigitValue = $el.val()[nowDigitIndex] ;
+
+        pMask = (typeof options.reverse == "boolean" && options.reverse === true) ?
+                __p.getProportionalReverseMask(oCleanedValue, mask) :
+                __p.getProportionalMask(oCleanedValue, mask);
+
+        if (nowDigitValue === mask[nowDigitIndex] && plugin.settings.maskChars[nowDigitValue]) {
+          $el.val(__p.cleanBullShit($el.val(), mask));
+          return true;
+        } 
+
+        oNewValue = __p.applyMask(e, $el, pMask, options);
+
+        if (oNewValue !== $el.val()){
+          $el.val(oNewValue).trigger('change');
+        }
+          
+        return __p.seekCallbacks(e, options, oNewValue, mask, $el);
+      },
+      applyMask: function (e, fieldObject, mask, options) {
+        var oValue = __p.onlyNonMaskChars(fieldObject.val()).substring(0, __p.onlyNonMaskChars(mask).length);
+        return __p.cleanBullShit(oValue.replace(new RegExp(__p.maskToRegex(mask)), function() {
+          for (var i = 1, oNewValue = ''; i < arguments.length - 2; i++) {
+            if (typeof arguments[i] == "undefined" || arguments[i] === "")
+              arguments[i] = mask.charAt(i-1);
+            oNewValue += arguments[i];
+          }
+
+          return oNewValue;
+        }), mask);
+      },
+      getProportionalMask: function (oValue, mask) {
+        var endMask = 0, m = 0;
+
+        while (m <= oValue.length-1){
+          while(plugin.settings.maskChars[mask.charAt(endMask)])
+            endMask++;
+          endMask++;
+          m++;
+        }
+
+        return mask.substring(0, endMask);
+      },
+      getProportionalReverseMask: function (oValue, mask) {
+        var startMask = 0, endMask = 0, m = 0;
+        startMask = (mask.length >= 1) ? mask.length : mask.length-1;
+        endMask = startMask;
+
+        while (m <= oValue.length-1) {
+          while (plugin.settings.maskChars[mask.charAt(endMask-1)])
+            endMask--;
+          endMask--;
+          m++;
+        }
+
+        endMask = (mask.length >= 1) ? endMask : endMask-1;
+        return mask.substring(startMask, endMask);
+      },
+      maskToRegex: function (mask) {
+        for (var i = 0, regex = ''; i < mask.length; i ++){
+          if (plugin.settings.specialChars[mask.charAt(i)])
+            regex += plugin.settings.specialChars[mask.charAt(i)];
+        }
+        return regex;
+      },
+      validDigit: function (nowMask, nowDigit) {
+        if (new RegExp(plugin.settings.specialChars[nowMask]).test(nowDigit) === false) {
+          return false;
+        }
+        return true;
+      },
+      cleanBullShit: function (oNewValue, mask, index) {
+        index = index || 0;
+        oNewValue = oNewValue.split('');
+        for (var i = index; i < mask.length; i++) {
+          if (__p.validDigit(mask.charAt(i), oNewValue[i]) === false && typeof oNewValue[i] !== "undefined") {
+            oNewValue[i] = '';
+            return __p.cleanBullShit(oNewValue.join(''), mask, 0);
+          } 
+        }
+        return oNewValue.join('');
+      },
+      seekCallbacks: function (e, options, oNewValue, mask, currentField) {
+        if (options.onKeyPress && e.isTrigger === undefined && typeof options.onKeyPress == "function") {
+          options.onKeyPress(oNewValue, e, currentField, options);
+        }
+        
+        if (options.onComplete && e.isTrigger === undefined &&
+            oNewValue.length === mask.length && typeof options.onComplete == "function") {
+          options.onComplete(oNewValue, e, currentField, options);
+        }
+      }
+    };
+
+    // qunit private methods testing
+    if (typeof QUNIT === "boolean") plugin.__p = __p;
 
     // public methods
     plugin.remove = function() {
-      destroyEvents();
-      $el.val(onlyNonMaskChars($el.val()));
-    };
-
-    // private methods
-    var resolveDynamicMask = function(mask, oValue, e, currentField, options){
-      return typeof mask == "function" ? mask(oValue, e, currentField, options) : mask;
-    };
-
-    var onlyNonMaskChars = function(string) {
-      $.each(plugin.settings.maskChars, function(k,v){
-        string = string.replace(new RegExp(plugin.settings.maskChars[k], 'g'), '')
-      });
-      return string;
-    };
-
-    var onPasteMethod = function(){
-      setTimeout(function(){
-        $el.trigger('keyup');
-      }, 100);
-    };
-
-    var setOnPaste = function() {
-      (hasOnSupport()) ? $el.on("paste", onPasteMethod) : $el.onpaste = onPasteMethod;
-    };
-
-    var setOnKeyUp = function(){
-      $el.keyup(maskBehaviour).trigger('keyup');
-    };
-
-    var hasOnSupport = function() {
-      return $.isFunction($.on);
-    };
-
-    var destroyEvents = function(){
-      $el.unbind('keyup').unbind('onpaste');
-    };
-
-    var maskBehaviour = function(e){
-      e = e || window.event;
-      keyCode = e.keyCode || e.which;
-
-      if ($.inArray(keyCode, plugin.settings.byPassKeys) > -1)  return true;
-
-      var oCleanedValue = onlyNonMaskChars($el.val()),
-          nowDigitIndex = $el.val().length-1,
-          nowDigitValue = $el.val()[nowDigitIndex] ;
-
-      pMask = (typeof options.reverse == "boolean" && options.reverse === true) ?
-              getProportionalReverseMask(oCleanedValue, mask) :
-              getProportionalMask(oCleanedValue, mask);
-
-      if (nowDigitValue === mask[nowDigitIndex] && plugin.settings.maskChars[nowDigitValue]) {
-        $el.val(cleanBullShit($el.val(), mask));
-        return true;
-      } 
-
-      oNewValue = applyMask(e, $el, pMask, options);
-
-      if (oNewValue !== $el.val()){
-        $el.val(oNewValue).trigger('change');
-      }
-        
-      return seekCallbacks(e, options, oNewValue, mask, $el);
-    };
-
-    var applyMask = function (e, fieldObject, mask, options) {
-      var oValue = onlyNonMaskChars(fieldObject.val()).substring(0, onlyNonMaskChars(mask).length);
-      return cleanBullShit(oValue.replace(new RegExp(maskToRegex(mask)), function() {
-        for (var i = 1, oNewValue = ''; i < arguments.length - 2; i++) {
-          if (typeof arguments[i] == "undefined" || arguments[i] === "")
-            arguments[i] = mask.charAt(i-1);
-          oNewValue += arguments[i];
-        }
-
-        return oNewValue;
-      }), mask);
-    };
-
-    var getProportionalMask = function (oValue, mask) {
-      var endMask = 0, m = 0;
-
-      while (m <= oValue.length-1){
-        while(plugin.settings.maskChars[mask.charAt(endMask)])
-          endMask++;
-        endMask++;
-        m++;
-      }
-
-      return mask.substring(0, endMask);
-    };
-
-    var getProportionalReverseMask = function (oValue, mask) {
-      var startMask = 0, endMask = 0, m = 0;
-      startMask = (mask.length >= 1) ? mask.length : mask.length-1;
-      endMask = startMask;
-
-      while (m <= oValue.length-1) {
-        while (plugin.settings.maskChars[mask.charAt(endMask-1)])
-          endMask--;
-        endMask--;
-        m++;
-      }
-
-      endMask = (mask.length >= 1) ? endMask : endMask-1;
-      return mask.substring(startMask, endMask);
-    };
-
-    var maskToRegex = function (mask) {
-      for (var i = 0, regex = ''; i < mask.length; i ++){
-        if (plugin.settings.specialChars[mask.charAt(i)])
-          regex += plugin.settings.specialChars[mask.charAt(i)];
-      }
-      return regex;
-    };
-
-    var validDigit = function (nowMask, nowDigit) {
-      if (new RegExp(plugin.settings.specialChars[nowMask]).test(nowDigit) === false) {
-        return false;
-      }
-      return true;
-    };
-
-    var cleanBullShit = function (oNewValue, mask, index) {
-      index = index || 0;
-      oNewValue = oNewValue.split('');
-      for (var i = index; i < mask.length; i++) {
-        if (validDigit(mask.charAt(i), oNewValue[i]) === false && typeof oNewValue[i] !== "undefined") {
-          oNewValue[i] = '';
-          return cleanBullShit(oNewValue.join(''), mask, 0);
-        } 
-      }
-      return oNewValue.join('');
-    };
-
-    var seekCallbacks = function (e, options, oNewValue, mask, currentField) {
-      if (options.onKeyPress && e.isTrigger === undefined && typeof options.onKeyPress == "function") {
-        options.onKeyPress(oNewValue, e, currentField, options);
-      }
-      
-      if (options.onComplete && e.isTrigger === undefined &&
-          oNewValue.length === mask.length && typeof options.onComplete == "function") {
-        options.onComplete(oNewValue, e, currentField, options);
-      }
+      __p.destroyEvents();
+      $el.val(__p.onlyNonMaskChars($el.val()));
     };
 
     plugin.init();
