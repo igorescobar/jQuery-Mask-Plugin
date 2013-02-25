c7d13392d6810d59627b23f32e630aa70b0f8f04
Igor Escobar <blog@igorescobar.com>
2013-02-24 22:01:15 -0300

    fuckin stupid comma.

--- jquery.mask.js
+++ jquery.mask.js
@@ -1,6 +1,6 @@
  /**
  * jquery.mask.js
- * @version: v0.7.1 
+ * @version: v0.7.2
  * @author: Igor Escobar
  *
  * Created by Igor Escobar on 2012-03-10. Please report any bug at http://blog.igorescobar.com
@@ -33,7 +33,7 @@
 
 (function ($) {
     "use strict";
-    var  e,
+    var  e;
     var Mask = function (el, mask, options) {
         var plugin = this,
             $el = $(el),
