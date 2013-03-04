c90bae0210ad32fc1f0f218d6e046573f17da735
Igor Escobar <blog@igorescobar.com>
2013-03-03 21:34:02 -0300

    upgrading .travis.yml information and making corrections to build it on travis CI

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+       qunit: {
+          files: ['test/index.html']
+        }
+    });
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  
+  // A convenient task alias.
+  grunt.registerTask('test', 'qunit');
+   
+};
