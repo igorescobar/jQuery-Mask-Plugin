f2389fec9e04ab1722d8f9870edc15797fa0536f
Igor Escobar <igor.escobar@abril.com.br>
2013-08-19 10:20:43 -0300

    adding de delete key to byPassKeys

--- jquery.mask.js
+++ jquery.mask.js
@@ -40,7 +40,7 @@
         jMask.init = function() {
             options = options || {};
 
-            jMask.byPassKeys = [8, 9, 37, 38, 39, 40];
+            jMask.byPassKeys = [8, 9, 37, 38, 39, 40, 46];
             jMask.maskChars = {':': ':', '-': '-', '.': '\\\.', '(': '\\(', ')': '\\)', '/': '\/', ',': ',', '_': '_', ' ': '\\s', '+': '\\\+'};
             jMask.translationNumbers = {0: '\\d', 1: '\\d', 2: '\\d', 3: '\\d', 4: '\\d', 5: '\\d', 6: '\\d', 7: '\\d', 8: '\\d', 9: '\\d'};
             jMask.translation = {'A': '[a-zA-Z0-9]', 'S': '[a-zA-Z]'};
