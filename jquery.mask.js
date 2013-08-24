a1da051dd60b5baaf7814839211494342b5d8c42
Igor Escobar <blog@igorescobar.com>
2013-08-24 15:58:59 -0300

    generating jquery mask files v1.1.0

--- jquery.mask.js
+++ jquery.mask.js
@@ -1,6 +1,6 @@
  /**
  * jquery.mask.js
- * @version: v1.0.3
+ * @version: v1.1.0
  * @author: Igor Escobar
  *
  * Created by Igor Escobar on 2012-03-10. Please report any bug at http://blog.igorescobar.com
