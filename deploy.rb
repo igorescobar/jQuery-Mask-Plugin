d2fd249f5f736e51763e181aa9f211783a0bd229
Igor Escobar <blog@igorescobar.com>
2014-08-08 00:55:19 -0300

    generating jquery mask files v1.7.1

--- deploy.rb
+++ deploy.rb
@@ -9,7 +9,7 @@ JMASK_GZIP_FILE = 'jquery.mask.min.js.gz'
 JMASK_VERSION = `stepup version --next-release`.delete("\n")
 BOWER_MANIFEST_FILE = 'bower.json'
 NPM_MANIFEST_FILE = 'package.json'
-COMPONENT_MANIFEST_FILE = 'package.json'
+COMPONENT_MANIFEST_FILE = 'component.json'
 
 abort("No notes, do deal.") if JMASK_VERSION.empty?
 
@@ -19,29 +19,37 @@ File.open(JMASK_FILE, 'w') do |file|
   file.write(unversioned_jmask_file.gsub(/\* @version: (v[0-9.+]+)/, "\* @version: #{JMASK_VERSION}"))
 end
 
-puts '# UPGRADING BOWER MANIFEST FILE'
-bower_manifest_file = File.open(BOWER_MANIFEST_FILE, 'rb') { |file| file.read }
-File.open(BOWER_MANIFEST_FILE, 'w') do |file|
-  file.write(bower_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
-end
+[BOWER_MANIFEST_FILE, NPM_MANIFEST_FILE, COMPONENT_MANIFEST_FILE, JQUERY_MANIFEST_FILE].each { |manifest_name|
+  puts "# UPGRADING #{manifest_name} "
+  manifest_file = File.open(manifest_name, 'rb') { |file| file.read }
+  File.open(manifest_name, 'w') do |file|
+    file.write(manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
+  end
+}
 
-puts '# UPGRADING PACKAGE.JSON MANIFEST FILE'
-npm_manifest_file = File.open(NPM_MANIFEST_FILE, 'rb') { |file| file.read }
-File.open(NPM_MANIFEST_FILE, 'w') do |file|
-  file.write(npm_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
-end
+# puts '# UPGRADING BOWER MANIFEST FILE'
+# bower_manifest_file = File.open(BOWER_MANIFEST_FILE, 'rb') { |file| file.read }
+# File.open(BOWER_MANIFEST_FILE, 'w') do |file|
+#   file.write(bower_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
+# end
 
-puts '# UPGRADING PACKAGE.JSON MANIFEST FILE'
-component_manifest_file = File.open(COMPONENT_MANIFEST_FILE, 'rb') { |file| file.read }
-File.open(COMPONENT_MANIFEST_FILE, 'w') do |file|
-  file.write(component_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
-end
+# puts '# UPGRADING PACKAGE.JSON MANIFEST FILE'
+# npm_manifest_file = File.open(NPM_MANIFEST_FILE, 'rb') { |file| file.read }
+# File.open(NPM_MANIFEST_FILE, 'w') do |file|
+#   file.write(npm_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
+# end
 
-puts '# UPGRADING JQUERY PLUGINS MANIFEST FILE'
-jquery_manifest_file = File.open(JQUERY_MANIFEST_FILE, 'rb') { |file| file.read }
-File.open(JQUERY_MANIFEST_FILE, 'w') do |file| 
-  file.write(jquery_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
-end
+# puts '# UPGRADING PACKAGE.JSON MANIFEST FILE'
+# component_manifest_file = File.open(COMPONENT_MANIFEST_FILE, 'rb') { |file| file.read }
+# File.open(COMPONENT_MANIFEST_FILE, 'w') do |file|
+#   file.write(component_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
+# end
+
+# puts '# UPGRADING JQUERY PLUGINS MANIFEST FILE'
+# jquery_manifest_file = File.open(JQUERY_MANIFEST_FILE, 'rb') { |file| file.read }
+# File.open(JQUERY_MANIFEST_FILE, 'w') do |file| 
+#   file.write(jquery_manifest_file.gsub(/"version": "([0-9.+]+)"/, "\"version\": \"#{JMASK_VERSION.gsub("v", "")}\""))
+# end
 
 puts '# GENERATING MIN FILE'
 jquery_mask_min_file = nil
@@ -78,6 +86,7 @@ puts '# UPGRATING CHANGELOG'
 
 puts '# UPGRADING gh-pages'
 `git checkout gh-pages`
+`git pull origin gh-pages` 
 
 minFile = File.open(GHPAGES_JMASK_MIN_FILE, 'w')
 minFile.puts("// jQuery Mask Plugin #{JMASK_VERSION}")
