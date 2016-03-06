5f5c3326d877c6c9eb549c7420e997da4dc7b80e
Igor Escobar <blog@igorescobar.com>
2016-03-06 23:12:40 +0000

    meteor manifest file

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'igorescobar:jquery-mask-plugin',
+  version: '1.13.7',
+  summary: 'A jQuery Plugin to make masks on form fields and HTML elements.',
+  git: 'https://github.com/igorescobar/jQuery-Mask-Plugin',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('dist/jquery.mask.js');
+  api.addFiles('dist/jquery.mask.min.js');
+});
