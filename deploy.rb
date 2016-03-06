bee66246fb8c0e51cd8f32eb654a6bd72faacba8
Igor Escobar <blog@igorescobar.com>
2016-03-06 23:22:32 +0000

    adding meteor to deploy workflow

--- deploy.rb
+++ deploy.rb
@@ -8,20 +8,21 @@ JMASK_GZIP_FILE = 'dist/jquery.mask.min.js.gz'
 JMASK_VERSION = `stepup version --next-release`.delete("\n")
 BOWER_MANIFEST_FILE = 'bower.json'
 NPM_MANIFEST_FILE = 'package.json'
+METEOR_MANIFEST_FILE = 'package.js'
 COMPONENT_MANIFEST_FILE = 'component.json'
 
 abort("No notes, do deal.") if JMASK_VERSION.empty?
 
 puts '# PUTTING NEW VERSION INSIDE OF JQUERY MASK FILE'
 unversioned_jmask_file = File.open(JMASK_FILE, 'rb') { |file| file.read }
-File.open(JMASK_FILE, 'w') do |file| 
+File.open(JMASK_FILE, 'w') do |file|
   file.write(unversioned_jmask_file.gsub(/\* @version: (v[0-9.+]+)/, "\* @version: #{JMASK_VERSION}"))
 end
 
 puts '# COPYING NEW JMASK FILE TO DIST/'
 `yes | cp #{JMASK_FILE} dist/`
 
-[BOWER_MANIFEST_FILE, NPM_MANIFEST_FILE, COMPONENT_MANIFEST_FILE].each { |manifest_name|
+[BOWER_MANIFEST_FILE, NPM_MANIFEST_FILE, COMPONENT_MANIFEST_FILE, METEOR_MANIFEST_FILE].each { |manifest_name|
   puts "# UPGRADING #{manifest_name} "
   manifest_file = File.open(manifest_name, 'rb') { |file| file.read }
   File.open(manifest_name, 'w') do |file|
@@ -31,10 +32,10 @@ puts '# COPYING NEW JMASK FILE TO DIST/'
 
 puts '# GENERATING MIN FILE'
 jquery_mask_min_file = nil
-File.open(JMASK_FILE, 'r') do |file| 
+File.open(JMASK_FILE, 'r') do |file|
   minFile = File.open(JMASK_MIN_FILE, 'w')
   minFile.puts("// jQuery Mask Plugin #{JMASK_VERSION}")
-  minFile.puts("// github.com/igorescobar/jQuery-Mask-Plugin") 
+  minFile.puts("// github.com/igorescobar/jQuery-Mask-Plugin")
   jquery_mask_min_file = `java -jar ../clojure-compiler/compiler.jar --js src/jquery.mask.js --charset UTF-8`
   minFile.puts(jquery_mask_min_file)
   minFile.close
@@ -64,19 +65,19 @@ puts '# UPGRATING CHANGELOG'
 
 puts '# UPGRADING gh-pages'
 `git checkout gh-pages`
-`git pull origin gh-pages` 
+`git pull origin gh-pages`
 
 minFile = File.open(GHPAGES_JMASK_MIN_FILE, 'w')
 minFile.puts("// jQuery Mask Plugin #{JMASK_VERSION}")
-minFile.puts("// github.com/igorescobar/jQuery-Mask-Plugin") 
+minFile.puts("// github.com/igorescobar/jQuery-Mask-Plugin")
 minFile.puts(jquery_mask_min_file)
-minFile.close 
+minFile.close
 
 `git commit -am "upgrading plugin file"`
 `git push`
 `git checkout master`
 
 puts '# PUBLISHING NPM PACKAGE'
-`npm publish` 
+`npm publish`
 
 puts '# DONE!'
--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  name: 'igorescobar:jquery-mask-plugin',
-  version: '1.13.7',
-  summary: 'A jQuery Plugin to make masks on form fields and HTML elements.',
-  git: 'git@github.com:igorescobar/jQuery-Mask-Plugin.git',
-  documentation: 'README.md'
+  "name": "igorescobar:jquery-mask-plugin",
+  "version": "1.13.7",
+  "summary": "A jQuery Plugin to make masks on form fields and HTML elements.",
+  "git": "git@github.com:igorescobar/jQuery-Mask-Plugin.git",
+  "documentation": "README.md"
 });
 
 Package.onUse(function(api) {
