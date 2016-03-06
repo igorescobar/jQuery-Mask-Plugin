98c4e0f5b434f8bec70d5ef60570b4477ec63486
Igor Escobar <blog@igorescobar.com>
2016-03-06 23:25:22 +0000

    generating jquery mask files v1.13.8

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "name": "igorescobar:jquery-mask-plugin",
-  "version": "1.13.7",
+  "version": "1.13.8",
   "summary": "A jQuery Plugin to make masks on form fields and HTML elements.",
   "git": "git@github.com:igorescobar/jQuery-Mask-Plugin.git",
   "documentation": "README.md"
