e1467fefd90ac496e0782b38339c132437e3dc15
Vasco Marques <vascogmm@gmail.com>
2017-05-21 17:21:15 +0100

    Makes donation bar collapsable after scroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 var elementPosition = $('.docs-nav').offset();
 
+var donationBar   = document.getElementsByClassName('donate')[0],
+    donationClose = donationBar.getElementsByTagName('i')[0];
+
 $(window).scroll(function(){
   if($(window).scrollTop() > elementPosition.top - 120){
     $('.docs-nav').addClass('fixed');
   } else {
     $('.docs-nav').removeClass('fixed');
   }
+
+  if ( $(window).scrollTop() > 600 ) {
+    donationClose.classList.remove('hidden');
+  } else {
+    donationClose.classList.add('hidden');
+  }
+
+  if ( $(window).scrollTop() == 0 ) {
+    donationBar.classList.remove('top');
+  }
 });
 
+donationClose.addEventListener('click', function() {
+  donationBar.classList.add('hidden');
+
+  setTimeout(function(){
+    donationBar.classList.add('top');
+    donationBar.classList.remove('hidden');
+  }, 200);
+});
 
 var codeNavigationLinks = $('.code-nav a'),
     codeTopic = document.getElementsByClassName('doc-point');
