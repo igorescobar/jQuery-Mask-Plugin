0989c9fa48d0f39c31f3337132b83f8ea06f53fc
Vasco Marques <vascogmm@gmail.com>
2017-05-30 23:50:25 +0100

    More changes to the new page

--- js/main.js
+++ js/main.js
@@ -3,24 +3,6 @@ var elementPosition = $('.docs-nav').offset();
 var donationBar   = document.getElementsByClassName('donate')[0],
     donationClose = donationBar.getElementsByTagName('i')[0];
 
-$(window).scroll(function(){
-  if($(window).scrollTop() > elementPosition.top - 120){
-    $('.docs-nav').addClass('fixed');
-  } else {
-    $('.docs-nav').removeClass('fixed');
-  }
-
-  if ( $(window).scrollTop() > 600 ) {
-    donationClose.classList.remove('hidden');
-  } else {
-    donationClose.classList.add('hidden');
-  }
-
-  if ( $(window).scrollTop() == 0 ) {
-    donationBar.classList.remove('top');
-  }
-});
-
 donationClose.addEventListener('click', function() {
   donationBar.classList.add('hidden');
 
@@ -60,3 +42,24 @@ var removeActiveCodeNavItem = function() {
       $(codeNavigationLinks[i]).removeClass('active');
   }
 }
+
+
+$(window).scroll(function(){
+  var position = $(this).scrollTop();
+
+  // if($(window).scrollTop() > elementPosition.top - 120){
+  //   $('.docs-nav').addClass('fixed');
+  // } else {
+  //   $('.docs-nav').removeClass('fixed');
+  // }
+
+  if ( $(window).scrollTop() > 600 ) {
+    donationClose.classList.remove('hidden');
+  } else {
+    donationClose.classList.add('hidden');
+  }
+
+  if ( $(window).scrollTop() == 0 ) {
+    donationBar.classList.remove('top');
+  }
+});
